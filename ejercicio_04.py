96d727e2fa4cbb82632742e682ebe06871deb7a9
jenima14 <jmarticarr2@educacion.navarra.es>
2023-10-17 22:21:52 +0200

    ejercicio_04

---
+++ ejercicio_04.py
@@ -0,0 +1,4 @@
+numero_telefono = input("Ingrese el número de teléfono con formato +34-XXXXXXXXX-XX: ")
+numero_principal = numero_telefono[4:13]
+print("El número de teléfono sin prefijo y sin extension es:", numero_principal)
+input()
