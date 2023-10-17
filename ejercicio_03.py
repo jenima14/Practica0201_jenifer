1eb505d3e5e19624544fda6e503718b235c75c82
jenima14 <jmarticarr2@educacion.navarra.es>
2023-10-17 21:08:51 +0200

    ejercicio_03

---
+++ ejercicio_03.py
@@ -0,0 +1,6 @@
+nombre = input("Introduce tu nombre: ")
+nombre_mayusculas = nombre.upper()
+numero_letras = len(nombre)
+resultado = nombre_mayusculas + " tiene " + str(numero_letras) + " letras."
+print(resultado)
+input()
